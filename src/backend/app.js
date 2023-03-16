9a848471162165cf57a7295105bb41433a159155
Dima-Kutaini <deemakutainy@gmail.com>
2023-03-16 16:22:06 +0100

    Add meal-shearing week1

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const path = require("path");
-
+const knex = require('./database');
 const mealsRouter = require("./api/meals");
 const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
@@ -20,6 +20,89 @@ app.use(express.json());
 app.use(cors());
 
 router.use("/meals", mealsRouter);
+router.get('/', (req, res) => {
+  res.send('Hi friend');
+});
+//Respond with all meals in the future
+//(relative to the when datetime):
+router.get('/future-meals', async (req, res) => {
+  try {
+    //const NOW = new Date();
+    const futureMeals = await knex.raw(
+      `
+  SELECT * FROM meals
+   WHERE date > NOW()`
+    );
+    res.json(futureMeals);
+  } catch (error) {
+    throw error;
+  }
+});
+//Respond with all meals in the past
+//(relative to the when datetime):
+router.get('/past-meals', async (req, res) => {
+  try {
+    //const now = new Date();
+    const pastMeals = await knex.raw(
+      `
+  SELECT * FROM meals
+   WHERE date < NOW()`
+    );
+
+    router.use('/meals', mealsRouter);
+    res.json(pastMeals);
+  } catch (error) {
+    throw error;
+  }
+});
+//Respond with all meals sorted by ID:
+router.get('/all-meals', async (req, res) => {
+  try {
+    const allMeals = await knex.raw(
+      `
+  SELECT * FROM meals
+  ORDER BY idmeals
+   `
+    );
+    res.json(allMeals);
+  } catch (error) {
+    throw error;
+  }
+});
+//Respond with the first meal (meaning with the minimum id):
+router.get('/first-meals', async (req, res) => {
+  try {
+    const firstMeals = await knex.raw(
+      `
+  SELECT * FROM meals
+  WHERE idmeals=( SELECT MIN(idmeals) FROM  meals)
+   `
+    );
+    if (firstMeals.length === 0) {
+      res.status(404).json({ error: 'There are no meals' });
+    }
+    res.json(firstMeals);
+  } catch (error) {
+    throw error;
+  }
+});
+router.get('/last-meals', async (req, res) => {
+  try {
+    const lastMeals = await knex.raw(
+      `
+  SELECT * FROM meals
+  WHERE idmeals=( SELECT MAX(idmeals) FROM  meals)
+   `
+    );
+    if (lastMeals.length === 0) {
+      res.status(404).json({ error: 'There are no meals' });
+    }
+    res.json(lastMeals);
+  } catch (error) {
+    throw error;
+  }
+});
+
 
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
